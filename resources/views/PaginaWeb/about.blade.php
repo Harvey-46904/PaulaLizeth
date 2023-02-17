517cd80ac1a45e27d1f0422382b86b1d62108717
Harvey-46904 <harveympv@hotmail.com>
2023-02-17 18:21:17 -0500

    lgo cambiado fin

--- resources/views/PaginaWeb/about.blade.php
+++ resources/views/PaginaWeb/about.blade.php
@@ -264,8 +264,8 @@
          
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <h4>Nuevas Noticias</h4>
+           
             <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form>
@@ -278,14 +278,14 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Groovin</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Constelarsana</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/groovin-free-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          Designed by <a href="">Mike-Mao92-Hache</a>
       </div>
     </div>
   </footer><!-- End Footer -->
