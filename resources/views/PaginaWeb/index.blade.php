159fcbdb4726bb157d527c47d50cd30beba9dac2
Yidwar <mikeyidwar@gmail.com>
2023-03-07 15:49:37 -0500

    nueva caja de comentarios

--- resources/views/PaginaWeb/index.blade.php
+++ resources/views/PaginaWeb/index.blade.php
@@ -677,13 +677,34 @@ Es distinto en el caso de los hijos menores de edad. Los padres pueden constelar
     <section id="comentario">
     <div class="container section-title pt-5">
     <h2>Comentarios</h2>
-
-    
-      <div class="row justify-content-center" >
+    <div class="row justify-content-center" >
+    <div class="col-md-10">
+        <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://paulacomentario.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    </div>
+    </div>
+      <!-- ======= <div class="row justify-content-center" >
         <div class="col-md-10">
         <div class="powr-comments" id="90ee4abe_1678203268"></div><script src="https://www.powr.io/powr.js?platform=html"></script>
         </div>
-      </div>
+      </div>======= -->
     </div>
     </section>
     <!-- ======= Contact Section ======= -->
